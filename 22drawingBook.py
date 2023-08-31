212635349670c74d6b2dd70ec278d2914dc55e3e
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2023-08-31 19:32:03 +0530

    Create 22drawingBook.py

---
+++ 22drawingBook.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    start=p//2
+    if (n%2)==1:
+        end=(n-p)//2
+    else:
+        end=(n-p+1)//2
+    return min(start,end)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
