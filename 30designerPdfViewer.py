4b60fec5723ab301662bf432bbe32c214efc99dc
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2023-10-25 13:24:05 +0530

    Create 30designerPdfViewer.py

---
+++ 30designerPdfViewer.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Calculate the maximum height of letters in the word
+    max_height = max(h[ord(char) - ord('a')] for char in word)
+    
+    # Calculate the area by multiplying max_height by the width of each character (which is 1)
+    area = max_height * len(word)
+    
+    return area
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
